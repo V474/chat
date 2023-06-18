565e41fe5101f42a104324961411268bfda61556
V474 <9835799@gmail.com>
2023-06-18 21:59:36 +0300

    Inheritance

--- main.js
+++ main.js
@@ -33,3 +33,33 @@ function factorialize(num) {
     }
   }
   
+
+  //Inheritance
+  function Cat(name) {
+    this.name = name;
+  }
+  
+  Cat.prototype = {
+    constructor: Cat,
+    eat: function() {
+      console.log("nom nom nom");
+    }
+  };
+  
+  function Bear(name) {
+    this.name = name;
+  }
+  
+  Bear.prototype = {
+    constructor: Bear,
+    eat: function() {
+      console.log("nom nom nom");
+    }
+  };
+  
+  function Animal() { }
+  
+  Animal.prototype = {
+    constructor: Animal,
+  
+  };
